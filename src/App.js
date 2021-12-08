0e5796eecd9bf84a876749ba1e616e9981de8c90
Christophe Daniel <cdaniel@easyvista.com>
2021-12-08 11:42:19 +0100

    composant Button non pure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useMemo, useCallback } from 'react';
+
+// composant Button non pure. Ajouter React.memo pour le rendre pure
+const Button = function({onClick}) {
+  console.log('<Button> render');
+  return <button onClick={onClick}>Mon bouton</button>
+};
+// composant Button pure
+const ButtonMemo = React.memo(function({onClick}) {
+  console.log('<Button> render');
+  return <button onClick={onClick}>Mon bouton</button>
+});
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  // pas optimisé. React considère que la fonction est différente à chaque rendu
+  const handleClick = function () {
+    alert('Bonjour');
+  };
+  // optimisé. On mémorise la fonction avec useMemo
+  const handleClickMemo = useMemo(function () {
+    return function () {
+      alert('Bonjour');
+    }
+  }, []);
+  // optimisé. On mémorise la fonction avec useCallback, version raccourcie de useMemo
+  const handleClickCallback = useCallback(function () {
+    alert('Bonjour');
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button onClick={handleClick}></Button>
+      <button onClick={() => setCount(c => c + 1)}>Incrémenter {count}</button>
     </div>
   );
 }
